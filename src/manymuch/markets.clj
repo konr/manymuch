b50d02d69be20617d19ec4c3f51dde04fcfb90ba
Konrad Scorciapino <konrad@eo2.com.br>
2014-01-23 23:38:32 -0200

    m-m-m-m-m-mercado bitcoin

--- src/manymuch/markets.clj
+++ src/manymuch/markets.clj
@@ -5,7 +5,10 @@
 
 (def markets
   [{:name :cryptsy
-    :url "http://pubapi.cryptsy.com/api.php?method=marketdatav2"}])
+    :url "http://pubapi.cryptsy.com/api.php?method=marketdatav2"}
+   {:name :mercado-bitcoin
+    :url-btc "https://www.mercadobitcoin.com.br/api/ticker/"
+    :url-ltc "https://www.mercadobitcoin.com.br/api/ticker_litecoin/"}])
 
 
 (defn get-json [url]
@@ -23,3 +26,14 @@
      :for (Double. (:lasttradeprice market))
      :broker :cryptsy}))
 
+(defmethod last-trades :mercado-bitcoin [{:keys [url-btc url-ltc]}]
+  (let [btc (get-json url-btc)
+        ltc (get-json url-ltc)]
+    [{:buy "BTC"
+      :with "BRL"
+      :for (get-in btc [:ticker :last])
+      :broker :mercado-bitcoin}
+     {:buy "LTC"
+      :with "BRL"
+      :for (get-in ltc [:ticker :last])
+      :broker :mercado-bitcoin}]))
