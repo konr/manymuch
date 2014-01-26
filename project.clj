d229fd536908ddad7c01fa371def1a38e5b10132
Konrad Scorciapino <konrad@eo2.com.br>
2014-01-26 16:42:51 -0200

    new name

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject shibeshibe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Figure out how much money you have in crypto currencies"
+  :url "http://github.com/konr/shibeshibe"
+  :license {:name "GPL"
+            :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [swiss-arrows "0.6.0"]
